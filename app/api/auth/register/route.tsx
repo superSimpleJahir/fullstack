b7bd0efec7aca3c623d9aa468bb99bb98fb3a65b
Md Jahirul Islam <supersimple.jahir@gmail.com>
2025-06-22 22:19:07 +0600

    Implement user registration endpoint with error handling

--- app/api/auth/register/route.tsx
+++ app/api/auth/register/route.tsx
@@ -1,3 +1,27 @@
 import { connectToDatabase } from "@/lib/db";
 import User from "@/models/User";
 import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(request: NextRequest) {
+  try {
+    const { email, password } = await request.json();
+    if (!email || !password) {
+      return NextResponse.json({ error: "Email and password are required" }, { status: 400 });
+    }
+
+    await connectToDatabase();
+
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return NextResponse.json({ error: "User already exists" }, { status: 409 });
+    }
+
+    await User.create({ email, password });
+
+    return NextResponse.json({ message: "User registered successfully" }, { status: 201 });
+  } catch (error) {
+    console.error("Error registering user", error);
+    return NextResponse.json({ error: "Failed to register User" }, { status: 500 });
+  }
+}
